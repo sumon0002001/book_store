ae72eff0f56cbf46ae0eef9945ae6c8a0622631e
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-04-23 02:21:02 +0600

    Fixed the actions in actions/index.js file

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import * as types from './actionTypes';
+
+export const createBook = (book) => ({
+    type: 'CREATE_BOOK',
+    book,
+});
+
+export const removeBook = (book) => ({
+    type: 'REMOVE_BOOK',
+    book,
+});
