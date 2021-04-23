5cd3b193050753f89ddd99c2adc85e4a440caf15
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-04-23 22:11:10 +0600

    Make the BooksForm component stateful

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,36 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createBook } from '../actions/index';
 
-const BooksForm = () => {
-  const categories = [
-    'Action',
-    'Biography',
-    'History',
-    'Horror',
-    'Kids',
-    'Learning',
-    'Sci-Fi',
-  ];
 
-  return (
-    <form>
-      <div>
+class BooksForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      category: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  handleSubmit = event => {
+    const { createBook } = this.props;
+    event.preventDefault();
+    createBook(this.state);
+    this.setState({ title: '', category: '' });
+  }
+
+  render() {
+    const categories = [
+      'Action',
+      'Biography',
+      'History',
+      'Horror',
+      'Kids',
+      'Learning',
+      'Sci-Fi',
+    ];
+
+    return (
+      <form>
         <div>
-          <input type="text" id="title" name="title" />
+          <div>
+            <input type="text" id="title" name="title" />
+          </div>
         </div>
-      </div>
-      <div>
-        <select id="category" name="category" value="">
-          {categories.map((category) => (
-            <option key={category} value={category}>
-              {category}
-            </option>
-          ))}
-        </select>
-      </div>
-      <button type="submit">Save Book</button>
-    </form>
-
-  );
+        <div>
+          <select id="category" name="category" value="">
+            {categories.map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+        <button type="submit">Save Book</button>
+      </form>
+    );
+  }
 };
 
-export default BooksForm;
+BooksForm.prototypes = {
+  createBook: PropTypes.func.isRequired,
+}
+
+const mapDispatchToProps = dispatch => ({
+  createBook: book => {dispatch(createBook(bool)); }
+})
+
+export default connect(null, mapDispatchToProps)(BooksForm);
