5e7d4ec06b34174060312b21d8e5788e1300e8e5
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-04-24 11:44:57 +0600

    Enhance the Redux store

---
+++ src/reducers/filter.js
@@ -0,0 +1,9 @@
+const filter = (state = { filter: 'All' }, action) => {
+    if (action.type === 'CHANGE_FILTER ') {
+        const newFilter = action.category === 'All' ? '' : action.category;
+        return newFilter;
+    }
+    return state;
+};
+
+export default filter;
